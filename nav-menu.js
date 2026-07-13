be985b0e8ebcbe35b3bb8e4df7dc5ef178271d13
computerpranks <daniel.kamienny@gmail.com>
2026-07-13 11:00:17 -0400

    Implement menu toggle functionality

---
+++ nav-menu.js
@@ -0,0 +1,10 @@
+const menuIcon = document.querySelector('.menu-icon');
+const menu = document.querySelector('.menu-popup');
+        
+menuIcon.addEventListener('click', () => {
+    menu.style.display = "block";
+});
+
+document.querySelector('#close-menu').addEventListener('click', () => {
+    menu.style.display = 'none';
+});
