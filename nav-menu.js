110d7cdeecde33d78e230c98152d0a9cb958ef28
computerpranks <daniel.kamienny@gmail.com>
2026-07-13 11:16:55 -0400

    Fix quotes in menu display toggle

--- nav-menu.js
+++ nav-menu.js
@@ -2,7 +2,7 @@ const menuIcon = document.querySelector('.menu-icon');
 const menu = document.querySelector('.menu-popup');
         
 menuIcon.addEventListener('click', () => {
-    menu.style.display = "block";
+    menu.style.display = 'block';
 });
 
 document.querySelector('#close-menu').addEventListener('click', () => {
